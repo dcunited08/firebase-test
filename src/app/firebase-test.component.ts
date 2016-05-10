56f21c5734d4da2bf70727ab4ee6449206a1c897
jdanielcook <jdanielcook@gmail.com>
2016-05-10 12:33:22 -0400

    mostly working

--- src/app/firebase-test.component.ts
+++ src/app/firebase-test.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
 
 @Component({
   moduleId: module.id,
@@ -6,6 +7,32 @@ import {Component} from '@angular/core';
   templateUrl: 'firebase-test.component.html',
   styleUrls: ['firebase-test.component.css']
 })
+
 export class FirebaseTestAppComponent {
   title = 'firebase-test works!  It is a joy to use!';
+  push: FirebaseListObservable <any[]>;
+  set: FirebaseObjectObservable <any>;
+  update: FirebaseObjectObservable <any>;
+
+  constructor(af: AngularFire) {
+    this.push = af.database.list('/webTest/push');
+    this.set = af.database.object('/webTest/set');
+    this.update = af.database.object('/webTest/update');
+  }
+
+  setSet(set: string) {
+    this.set.set(set);
+  }
+
+  updateUpdate(pemdos: string) {
+
+  }
+
+  pushPush(test1: string, test2: string) {
+    this.push.push({
+      test1: test1,
+      test2: test2
+    });
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { FirebaseTestAppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(FirebaseTestAppComponent);
+bootstrap(FirebaseTestAppComponent, [
+  FIREBASE_PROVIDERS,
+  defaultFirebase('https://dcook-test1.firebaseio.com')
+]);
